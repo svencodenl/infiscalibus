68f86f8a306d5e994781a7484620e3e55174eb80
Sven Gaertman <sven@svencode.nl>
2024-07-15 14:49:41 +0200

    Remove unused folders

--- tests/e2e/example.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('footer contains "Built with Radicle"', async ({ page }) => {
-  await page.goto('/');
-  const footer = await page.$('footer');
-  expect(await footer?.textContent()).toContain('Built with Radicle');
-});
--- tests/e2e/helpers/loginHelper.js
+++
@@ -1,7 +0,0 @@
-export async function login(page) {
-  await page.goto('/wp/wp-login.php');
-  await page.fill('#user_login', 'admin');
-  await page.fill('#user_pass', 'admin');
-  await page.click('#wp-submit');
-  await page.waitForNavigation();
-}
