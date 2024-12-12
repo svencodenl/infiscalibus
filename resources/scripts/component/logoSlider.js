7b339f61dba619d58cf2468ac35eb005bc6568a8
Sven Gaertman <sven@svencode.nl>
2024-12-12 11:35:11 +0100

    Mobile styling logo slider

--- resources/scripts/component/logoSlider.js
+++ resources/scripts/component/logoSlider.js
@@ -5,6 +5,8 @@ export default function logoSlider() {
   const containers = document.querySelectorAll('.logo-slider-container');
   if (!containers) return;
 
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
   containers.forEach((container, index) => {
     // Create array of elements to tween on
     const items = gsap.utils.toArray('.logo-slider-item', container);
@@ -13,7 +15,7 @@ export default function logoSlider() {
     const loop = horizontalLoop(items, {
       paused: true, // Sets the tween to be paused initially
       repeat: -1, // Makes sure the tween runs infinitely
-      speed: 1 + index * 0.25,
+      speed: isMobile ? 0.5 + index * 0.20 : 1 + index * 0.25,
       draggable: true,
       paddingRight: parseFloat(gsap.getProperty(items[0], "marginRight", "px")) * 2,
     });
--- resources/views/flexible/logo_slider.blade.php
+++ resources/views/flexible/logo_slider.blade.php
@@ -20,12 +20,14 @@ $cpt_label = &$wp_post_types[$post_type]->labels->name;
 			<h2 class="heading">{{ $heading }}</h2>
 			@endif
 
-			<a href="{{ get_post_type_archive_link($post_type) }}">
-				<button class="btn btn-ir">
+			<button class="btn btn-ir">
+				<a href="{{ get_post_type_archive_link($post_type) }}">
 					Alle {{ $cpt_label }}
-					<i><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" /></svg></i>
-				</button>
-			</a>
+					<i><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px">
+							<path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+						</svg></i>
+				</a>
+			</button>
 		</div>
 		<div class="logo-slider-container">
 			<div class="logo-slider-container-inner">
