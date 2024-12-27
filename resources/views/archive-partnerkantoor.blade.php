349bfb5b2f3d3ff02b70e63cd08aa41578a94f6d
Sven Gaertman <sven@svencode.nl>
2024-12-27 14:00:18 +0100

    Partnerkantoren archive

--- resources/views/archive-partnerkantoor.blade.php
+++ resources/views/archive-partnerkantoor.blade.php
@@ -4,10 +4,24 @@
 @include('partials.page-header')
 
 <div class="container">
-	@while(have_posts()) @php(the_post())
-	  	@includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
-	@endwhile
+	<div class="row">
+		@while(have_posts()) @php(the_post())
+		{{-- @includeFirst(['partials.content-' . get_post_type(), 'partials.content']) --}}
+		<div class="col-4">
+			<div class="card">
+				<a href="{{ get_permalink() }}">
+					<div class="img-wrapper">
+						@if ($image = get_the_post_thumbnail_url() )
+						<img src="{{ $image }}" alt="{{ get_the_title() }}">
+						@else
+						<h3 class="heading">{{ get_the_title() }}</h3>
+						@endif
+					</div>
+				</a>
+			</div>
+		</div>
+		@endwhile
+	</div>
 </div>
 
 @endsection
-
