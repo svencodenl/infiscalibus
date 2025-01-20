7894791a0dd24ebe3d202dabd6eb704cf4712b2a
Sven Gaertman <sven@svencode.nl>
2025-01-20 17:25:50 +0100

    Order partnerkantoren archive alphabetical

--- resources/views/archive-partnerkantoor.blade.php
+++ resources/views/archive-partnerkantoor.blade.php
@@ -1,3 +1,13 @@
+@php
+$partnerkantoren = get_posts([
+'post_type' => 'partnerkantoor',
+'post_status' => 'publish',
+'posts_per_page' => -1,
+'orderby' => 'title',
+'order' => 'ASC',
+]);
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
@@ -5,22 +15,22 @@
 
 <div class="container">
 	<div class="row">
-		@while(have_posts()) @php(the_post())
+		@foreach ($partnerkantoren as $key => $item)
 		{{-- @includeFirst(['partials.content-' . get_post_type(), 'partials.content']) --}}
 		<div class="col-md-4 col-6">
 			<div class="card">
-				<a href="{{ get_permalink() }}">
+				<a href="{{ get_permalink($item->ID) }}">
 					<div class="img-wrapper">
-						@if ($image = get_the_post_thumbnail_url() )
-						<img src="{{ $image }}" alt="{{ get_the_title() }}">
+						@if ($image = get_the_post_thumbnail_url($item->ID) )
+						<img src="{{ $image }}" alt="{{ get_the_title($item->ID) }}">
 						@else
-						<h3 class="heading">{{ get_the_title() }}</h3>
+						<h3 class="heading">{{ get_the_title($item->ID) }}</h3>
 						@endif
 					</div>
 				</a>
 			</div>
 		</div>
-		@endwhile
+		@endforeach
 	</div>
 </div>
 
