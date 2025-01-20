2ebb6ac2d3efaaa1c50c224932f86310a172476a
Sven Gaertman <sven@svencode.nl>
2025-01-20 17:20:34 +0100

    Menu notification

--- resources/views/sections/navbar.blade.php
+++ resources/views/sections/navbar.blade.php
@@ -32,14 +32,24 @@ $nested_nav = recursive_mitems_to_array($primary_nav);
 	@endif
 	<div class="navbar-main">
 		<div class="container navbar-main-inner">
-			<a class="logo" href="{{ home_url('/') }}">
-				@if($logo_light = get_field('logo_light', 'option')['url'])
-				<img class="logo-light" src="{{ $logo_light }}" alt="F.S.V. In Fiscalibus">
+			<div class="left-side">
+				<a class="logo" href="{{ home_url('/') }}">
+					@if($logo_light = get_field('logo_light', 'option')['url'])
+					<img class="logo-light" src="{{ $logo_light }}" alt="F.S.V. In Fiscalibus">
+					@endif
+					@if($logo_dark = get_field('logo_regular', 'option')['url'])
+					<img class="logo-dark" src="{{ $logo_dark }}" alt="F.S.V. In Fiscalibus">
+					@endif
+				</a>
+				@if (($notification = get_field('menu_notification', 'option')) && $notification['active'])
+				<a class="menu-notification" href="{{ $notification['link'] }}">
+					{{ $notification['text'] }}
+					<i><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="24px">
+						<path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+					</svg></i>
+				</a>
 				@endif
-				@if($logo_dark = get_field('logo_regular', 'option')['url'])
-				<img class="logo-dark" src="{{ $logo_dark }}" alt="F.S.V. In Fiscalibus">
-				@endif
-			</a>
+			</div>
 
 			<div class="mobile-handler">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
