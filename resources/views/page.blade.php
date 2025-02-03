146b28c24772553bc11f6411906012e92b5e29ae
Sven Gaertman <sven@svencode.nl>
2025-02-03 17:03:38 +0100

    Fix content page layout

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,7 +3,15 @@
 @section('content')
     @while(have_posts()) @php(the_post())
         @include('partials.page-header')
-        @includeFirst(['partials.content-page', 'partials.content'])
+        <section class="section content-page">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-8">
+                        @includeFirst(['partials.content-page', 'partials.content'])
+                    </div>
+                </div>
+            </div>
+        </section>
         @include('flexible.flexible-content')
     @endwhile
 @endsection
--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,9 +1,1 @@
-<section class="section content-page">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-8">
-                @php(the_content())
-            </div>
-        </div>
-    </div>
-</section>
+@php(the_content())
