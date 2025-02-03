bc0b2200247bc73223b36bd9c2ea856c668ed427
Sven Gaertman <sven@svencode.nl>
2025-02-03 21:01:49 +0100

    Style single events

--- resources/views/single-evenement.blade.php
+++ resources/views/single-evenement.blade.php
@@ -18,86 +18,127 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 				<div class="sidebar-inner">
 					{{-- Event date & time --}}
 					@if ($start_date = get_field('event_date_start_date'))
-					<p>Start:
-						{{ $start_date }}
-						@if (($start_time = get_field('event_date_start_time')) && !get_field('event_date_is_entire_day'))
-						@ {{ $start_time }}
-						@endif
-					</p>
+					<div class="event-meta-item">
+						<p class="heading">Datum</p>
+						<p class="content">
+							{{ $start_date }}
+							@if (($start_time = get_field('event_date_start_time')) && !get_field('event_date_is_entire_day'))
+							@ {{ $start_time }}
+							@endif
+							@endif
+							@if ($end_date = get_field('event_date_end_date'))
+							-
+							{{ $end_date }}
+							@if (($end_time = get_field('event_date_end_time')) && !get_field('event_date_is_entire_day'))
+							@ {{ $end_time }}
+							@endif
+						</p>
+					</div>
 					@endif
-					@if ($end_date = get_field('event_date_end_date'))
-					<p>End:
-						{{ $end_date }}
-						@if (($end_time = get_field('event_date_end_time')) && !get_field('event_date_is_entire_day'))
-						@ {{ $end_time }}
-						@endif
-					</p>
-					@endif
-					</p>
 
 					{{-- Event register deadline --}}
 					@if (!get_field('event_register_no_deadline'))
-					@if ($start_register = get_field('event_register_start_register'))
-					<p>Start register: {{ $start_register }}</p>
-					@endif
-					@if ($end_register = get_field('event_register_end_register'))
-					<p>End register: {{ $end_register }}</p>
-					@endif
+					<div class="event-meta-item">
+						<p class="heading">Registreren vanaf - tot</p>
+						<p class="content">
+							@if ($start_register = get_field('event_register_start_register'))
+							{{ $start_register }}
+							@endif
+							@if ($end_register = get_field('event_register_end_register'))
+							- {{ $end_register }}
+							@endif
+						</p>
+					</div>
 					@endif
 
 					{{-- Declaratie tekst --}}
 					@if ($declare_text = get_field('declare_text'))
-					<p>Declaratie tekst: {{ $declare_text }}</p>
+					<div class="event-meta-item">
+						<p class="heading">Declareren</p>
+						<p class="content">{{ $declare_text }}</p>
+					</div>
 					@endif
 
 					{{-- Dresscode tekst --}}
 					@if ($dresscode = get_field('dresscode'))
-					<p>Dresscode: {{ $dresscode }}</p>
+					<div class="event-meta-item">
+						<p class="heading">Dresscode</p>
+						<p class="content">{{ $dresscode }}</p>
+					</div>
 					@endif
 
 					{{-- Max. Capaciteit --}}
 					@if ($max_cap = get_field('capacity'))
-					<p>Max capacity: {{ $max_cap }}</p>
+					<div class="event-meta-item">
+						<p class="heading">Capaciteit</p>
+						<p class="content">{{ $max_cap }}</p>
+					</div>
 					@endif
 
 					{{-- Logistieke info --}}
 					@if ($logistic_info = get_field('logistic_info'))
-					<p>Logistic info: {{ $logistic_info }}</p>
+					<div class="event-meta-item">
+						<p class="heading">Logistieke info</p>
+						<p class="content">{{ $logistic_info }}</p>
+					</div>
 					@endif
 
 					{{-- Categories --}}
 					@if ($categories = get_the_terms(get_the_ID(), 'category-event'))
-					<p>Categories:
-						@foreach ($categories as $cat)
-						{{ $cat->name }}
-						@endforeach
-					</p>
+					<div class="event-meta-item">
+						<p class="heading">Categories</p>
+						<p class="content">
+							@foreach ($categories as $cat)
+							{{ $cat->name }}
+							@endforeach
+						</p>
+					</div>
 					@endif
 
 					{{-- Location --}}
 					@if ($location = get_the_terms(get_the_ID(), 'location'))
-					<p>Location: {{ $location[0]->name }}</p>
-					@if ($google_maps = get_field('google_maps', 'location' . '_' . $location[0]->term_id))
-					<pre>{{ var_dump($google_maps) }}</pre>
-					@endif
+					<div class="event-meta-item">
+						<p class="heading">Locatie</p>
+						<p class="content">{{ $location[0]->name }}</p>
+						@if ($google_maps = get_field('google_maps', 'location' . '_' . $location[0]->term_id))
+						<pre>{{ var_dump($google_maps) }}</pre>
+						@endif
+					</div>
 					@endif
 
 					{{-- Partnerkantoren --}}
 					@if ($partnerkantoren = get_field('event_partnerkantoren'))
-					<div class="sidebar-partnerkantoren">
-						<p>Partnerkantoren:</p>
-						@foreach($partnerkantoren as $key => $kantoor_id)
-						<a href="{{ get_permalink($kantoor_id) }}" class="kantoor-item">
-							<div class="kantoor-logo">
-								<img src="{{ wp_get_attachment_image_src( get_post_thumbnail_id($kantoor_id), 'single-post-thumbnail' )[0] }}" alt="{{ get_the_title($kantoor_id) }}">
-							</div>
-						</a>
-						@endforeach
+					<div class="event-meta-item">
+						<p class="heading">Partnerkantoren</p>
+						<div class="sidebar-partnerkantoren">
+							@foreach($partnerkantoren as $key => $kantoor_id)
+							<a href="{{ get_permalink($kantoor_id) }}" class="kantoor-item">
+								<div class="kantoor-logo">
+									<img
+										src="{{ wp_get_attachment_image_src( get_post_thumbnail_id($kantoor_id), 'single-post-thumbnail' )[0] }}"
+										alt="{{ get_the_title($kantoor_id) }}">
+								</div>
+							</a>
+							@endforeach
+						</div>
 					</div>
 					@endif
 
+					<pre>{{ var_dump(wp_get_current_user()) }}</pre>
+
 					@if($show_register_button)
-					<button class="btn btn--orange-primary">Register</button>
+					<form action="" method="post">
+						@csrf
+						<!-- Bescherming tegen CSRF -->
+						<input type="hidden" name="event_id" value="{{ get_the_ID() }}">
+						<input type="hidden" name="user_id" value="{{ wp_get_current_user()->ID }}">
+						<input type="hidden" name="user_name" value="{{ wp_get_current_user()->user_nicename }}">
+						<input type="hidden" name="user_email" value="{{ wp_get_current_user()->user_email }}">
+						<button type="submit" class="btn btn--orange-primary">Meld je aan</button>
+						@if (isset($_GET['success']) && $_GET['success'] == 'true')
+						<p class="success-message">Je aanmelding is succesvol verwerkt!</p>
+						@endif
+					</form>
 					@endif
 
 				</div>
