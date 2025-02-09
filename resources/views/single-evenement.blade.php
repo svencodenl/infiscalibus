b96e068e6fe736d07081f82b59896d7fc2a14acd
Sven Gaertman <sven@svencode.nl>
2025-02-09 11:09:36 +0100

    Evenementen logic to show when event is full and add terms and conditions text

--- resources/views/single-evenement.blade.php
+++ resources/views/single-evenement.blade.php
@@ -51,6 +51,36 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 					</div>
 					@endif
 
+					{{-- Max. Capaciteit --}}
+					@if ($max_cap = get_field('capacity'))
+					<div class="event-meta-item">
+						<p class="heading">Capaciteit</p>
+						<p class="content">
+							@if ($current_registrations = get_event_registration_count(get_the_ID()))
+								@if ($current_registrations >= $max_cap)
+									<span class="text--error"><strong>Dit evenement zit vol!</strong></span>
+								@else
+									{{ $max_cap - $current_registrations }}
+								@endif
+							@else
+								{{ $max_cap }}
+							@endif
+						</p>
+					</div>
+					@endif
+
+					{{-- Categories --}}
+					@if ($categories = get_the_terms(get_the_ID(), 'category-event'))
+					<div class="event-meta-item">
+						<p class="heading">Categorieën</p>
+						<p class="content">
+							@foreach ($categories as $cat)
+							{{ $cat->name }}
+							@endforeach
+						</p>
+					</div>
+					@endif
+
 					{{-- Declaratie tekst --}}
 					@if ($declare_text = get_field('declare_text'))
 					<div class="event-meta-item">
@@ -67,21 +97,15 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 					</div>
 					@endif
 
-					{{-- Max. Capaciteit --}}
-					@if ($max_cap = get_field('capacity'))
+					{{-- Location --}}
+					@if ($location = get_the_terms(get_the_ID(), 'location'))
 					<div class="event-meta-item">
-						<p class="heading">Capaciteit</p>
-						<p class="content">
-							@if ($current_registrations = get_event_registration_count(get_the_ID()))
-								@if ($current_registrations >= $max_cap)
-									<span style="color: red; font-weight: bold;">Dit evenement zit vol!</span>
-								@else
-									{{ $max_cap - $current_registrations }}
-								@endif
-							@else
-								{{ $max_cap }}
-							@endif
-						</p>
+						<p class="heading">Locatie</p>
+						<p class="content">{{ $location[0]->name }}</p>
+						@if ($google_maps = get_field('google_maps', 'location' . '_' . $location[0]->term_id))
+						{{-- <pre>{{ var_dump($google_maps) }}</pre> --}}
+						<a href="https://www.google.com/maps/search/?api=1&query={{ str_replace(' ', '+', $google_maps['address']) }}">{{ $google_maps['address'] }}</a>
+						@endif
 					</div>
 					@endif
 
@@ -90,30 +114,6 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 					<div class="event-meta-item">
 						<p class="heading">Logistieke info</p>
 						<p class="content">{{ $logistic_info }}</p>
-					</div>
-					@endif
-
-					{{-- Categories --}}
-					@if ($categories = get_the_terms(get_the_ID(), 'category-event'))
-					<div class="event-meta-item">
-						<p class="heading">Categories</p>
-						<p class="content">
-							@foreach ($categories as $cat)
-							{{ $cat->name }}
-							@endforeach
-						</p>
-					</div>
-					@endif
-
-					{{-- Location --}}
-					@if ($location = get_the_terms(get_the_ID(), 'location'))
-					<div class="event-meta-item">
-						<p class="heading">Locatie</p>
-						<p class="content">{{ $location[0]->name }}</p>
-						@if ($google_maps = get_field('google_maps', 'location' . '_' . $location[0]->term_id))
-						{{-- <pre>{{ var_dump($google_maps) }}</pre> --}}
-						<span>{{ $google_maps['address'] }}</span>
-						@endif
 					</div>
 					@endif
 
@@ -141,6 +141,8 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 						@php(wp_nonce_field('delete_registration_' . $registration_id, 'delete_registration_nonce'))
 						<button type="submit" name="delete_registration" class="btn btn--blue-primary">Afmelden</button>
 					</form>
+					@elseif(get_field('capacity') && (get_event_registration_count(get_the_ID()) >= get_field('capacity')))
+					<button type="submit" disabled class="btn">Evenement is vol</button>
 					@elseif($show_register_button)
 					<form action="" method="post">
 						@csrf
@@ -150,6 +152,9 @@ $show_register_button = is_allowed_to_register_to_event(get_the_ID());
 						<input type="hidden" name="user_name" value="{{ wp_get_current_user()->user_nicename }}">
 						<input type="hidden" name="user_email" value="{{ wp_get_current_user()->user_email }}">
 						<button type="submit" class="btn btn--orange-primary">Meld je aan</button>
+						@if($terms = get_field('terms_and_conditions_link', 'option'))
+						<p class="terms-text text--text-muted">Door aan te melden ga ik akkoord met de <a href="{{ $terms }}" target="_blank">Algemene voorwaarden</a></p>
+					@endif
 					</form>
 					@endif
 
